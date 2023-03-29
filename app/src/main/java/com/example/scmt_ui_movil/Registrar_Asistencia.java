71146bb155c8a539c2a9619dcd3ce9df4e59fad6
Abraham Cocoletzi Z <abraham_cryter2007@hotmail.com>
2023-03-29 16:27:29 -0600

    Interfaces Incidencias, Registro

---
+++ app/src/main/java/com/example/scmt_ui_movil/Registrar_Asistencia.java
@@ -0,0 +1,13 @@
+package com.example.scmt_ui_movil;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registrar_Asistencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_asistencia);
+    }
+}
